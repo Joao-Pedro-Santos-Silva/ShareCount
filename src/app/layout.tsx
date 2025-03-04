da3bb1c9f4eae374771611e6225953dd1f4477fd
Tederr <jp3351207@gmail.com>
2025-03-04 01:34:26 -0300

    Parte 1

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Montserrat, Oxanium } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "ShareCount",
+  icons: "/favicon.svg",
+};
+
+const montserrat = Montserrat({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+const oxanium = Oxanium({
+  weight: ["500", "600"],
+  subsets: ["latin"],
+  variable: "--font-oxanium",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${montserrat.variable} ${oxanium.variable}`}>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat md:bg-right-top bg-top">
+        {children}
+      </body>
+    </html>
+  );
+}
