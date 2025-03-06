0c644c32639468480ce04ed9c9c23d744b32fc34
Tederr <jp3351207@gmail.com>
2025-03-06 00:08:09 -0300

    Update page.tsx

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -12,7 +12,7 @@ interface InvitePageProps {
 export default async function InvitePage(props: InvitePageProps) {
 	const { subscriberId } = await props.params
 
-	const inviteLink = `http://localhost:3333/invites/${subscriberId}`
+	const inviteLink = `https://share-count.vercel.app/invites/${subscriberId}`
 
 	return (
 		<div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
